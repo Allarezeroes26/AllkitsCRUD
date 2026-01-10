21c8dc901a896a0fe72552842cea8a529b6e0eac
Allarezeroes26 <johnerwibacani26@gmail.com>
2026-01-10 20:57:20 +0800

    add

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ connectCloudinary()
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ]
 }))
 
 app.use('/api/products', productRoutes)
