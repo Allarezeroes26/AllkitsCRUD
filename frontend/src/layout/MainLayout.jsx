8ce3544732c1af4027f702e42a6cd35b91e16ffa
Allarezeroes26 <johnerwibacani26@gmail.com>
2026-01-18 14:52:50 +0800

    nav

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -4,7 +4,6 @@ import Footer from '../components/Footer'
 import Searchbar from '../components/Searchbar'
 import { ToastContainer, toast } from 'react-toastify';
 
-
 const MainLayout = () => {
   return (
     <div className='flex flex-col gap-10 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
